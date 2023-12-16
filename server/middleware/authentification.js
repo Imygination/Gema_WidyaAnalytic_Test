0c192f4f7425ae2c5aa04433cceed4bc3684abc6
Gema <gemaramadhan20@gmail.com>
2023-12-17 01:09:59 +0700

    add show product handler, authentification, and error handler

---
+++ server/middleware/authentification.js
@@ -0,0 +1,18 @@
+const { verifyToken } = require("../helper/jwt");
+const { User } = require("../models");
+
+async function authentification(req, res, next) {
+  try {
+    const payload = verifyToken(req.headers.access_token);
+    const user = User.findByPk(payload.id);
+    if (!user) {
+      throw { name: "Unauthentification" };
+    }
+    req.user = { id: user.id, email: user.email };
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = authentification;
--- server/routers/index.js
+++ server/routers/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const routerUser = require("./user");
 const routerProduct = require("./product");
+const authentification = require("../middleware/Authentification");
 
 router.use("/user", routerUser);
+
+router.use(authentification);
 router.use("/product", routerProduct);
 
 module.exports = router;

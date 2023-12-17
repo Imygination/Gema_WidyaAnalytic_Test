3827b8eac9fc60a7846b12dd1ecd3ecebc1bd195
Gema <gemaramadhan20@gmail.com>
2023-12-17 13:32:49 +0700

    add Delete Product Handler and Error Handler

--- server/routers/product.js
+++ server/routers/product.js
@@ -5,7 +5,6 @@ const router = express.Router();
 router.get("/", controllerProduct.showProducts);
 router.post("/", controllerProduct.createProduct);
 router.put("/:id", controllerProduct.updateProduct);
-router.get("/:id");
-router.delete("/:id");
+router.delete("/:id", controllerProduct.deleteProduct);
 
 module.exports = router;
